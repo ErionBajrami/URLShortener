805b04ad0397169260f99134d71956417d56c38a
TR1CKEY <fati_m1m@hotmail.com>
2024-04-17 15:05:32 +0200

    Update TokenService.cs

--- BackEnd/URLShortener/Service/TokenService.cs
+++ BackEnd/URLShortener/Service/TokenService.cs
@@ -3,14 +3,14 @@ using System.Security.Claims;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
 
-namespace MovieApp.Services;
+namespace URLShortener.Services;
 
 public class TokenService
 {
     public static string GenerateToken(int id, string email, string username)
     {
         var tokenHandler = new JwtSecurityTokenHandler();
-        var key = Encoding.ASCII.GetBytes("YOUR_SECRET_KEY_FROM_LIFE_GJIRAFA"); // Replace with your secret key
+        var key = Encoding.ASCII.GetBytes("LIFE_FROM_GJIRAFA"); 
         var tokenDescriptor = new SecurityTokenDescriptor
         {
             Subject = new ClaimsIdentity(new[] {
@@ -29,7 +29,7 @@ public class TokenService
     public static ClaimsPrincipal VerifyToken(string token)
     {
         var tokenHandler = new JwtSecurityTokenHandler();
-        var key = Encoding.ASCII.GetBytes("YOUR_SECRET_KEY_FROM_LIFE_GJIRAFA"); // Use the same secret key used to generate the token
+        var key = Encoding.ASCII.GetBytes("LIFE_FROM_GJIRAFA"); 
         try
         {
             var principal = tokenHandler.ValidateToken(token, new TokenValidationParameters
